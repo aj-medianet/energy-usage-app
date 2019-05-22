b81325b6162b013f1214aa5a9734f407ead29d42
Andrew Joseph <andrew@aj-media.net>
2019-05-22 16:56:48 -0600

    update

--- create-software-engineering-db.sql
+++ create-software-engineering-db.sql
@@ -5,10 +5,11 @@ CREATE TABLE `devices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `manufacturer` varchar(255) NOT NULL,
+  `deviceOnOff` int(11) NOT NULL,
   `currentEnergyUsage` int(11) NOT NULL,
   
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `devices` VALUES (1, 'Microwave', 'Toshiba',0), (2, 'Oven', 'Samsung',0), (3, 'Refrigerator', 'Samsung', 10), (4, 'Thermostat', 'Nest', 15);
+INSERT INTO `devices` VALUES (1, 'Microwave', 'Toshiba',0,0), (2, 'Oven', 'Samsung',0,0), (3, 'Refrigerator', 'Samsung',0, 10), (4, 'Thermostat', 'Nest',0, 15);
