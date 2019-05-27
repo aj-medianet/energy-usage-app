1ea569fe87a8e7c462b9842d08d502f7c2d9af07
Andrew Joseph <andrew@aj-media.net>
2019-05-27 09:11:32 -0600

    updated db fields

--- create-software-engineering-db.sql
+++ create-software-engineering-db.sql
@@ -13,4 +13,4 @@ CREATE TABLE `devices` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `devices` VALUES (1, 'Microwave', 'Toshiba',0,0,150), (2, 'Oven', 'Samsung',0,0,150), (3, 'Refrigerator', 'Samsung',0, 10,150), (4, 'Thermostat', 'Nest',1, 15,150), (5, 'A/C', 'Samsung',0, 210,150), (6, 'Dishwasher', 'Bosch',1, 120,150);
+INSERT INTO `devices` VALUES (1, 'Microwave', 'Toshiba',0,0,150), (2, 'Oven', 'Samsung',0,0,150), (3, 'Refrigerator', 'Samsung',1, 10,150), (4, 'Thermostat', 'Nest',1, 15,150), (5, 'A/C', 'Samsung',1, 210,150), (6, 'Dishwasher', 'Bosch',1, 120,150);
